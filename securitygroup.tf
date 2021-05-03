196cc3ee6c2a3ecebfc68ba01e509e6bf819cc9f
onesider <onesider@gmail.com>
2021-05-03 09:51:49 +0900

    Create securitygroup.tf

---
+++ securitygroup.tf
@@ -0,0 +1,24 @@
+resource "aws_default_security_group" "redteam_default" {
+  vpc_id      = aws_vpc.redteam-vpc.id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
